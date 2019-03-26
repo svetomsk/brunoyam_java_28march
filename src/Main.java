080e377bf13d8aa4eb718fbd9f017839ba5227b5
svetomsk <svt2404@gmail.com>
2019-03-26 22:25:18 +0300

    last changes

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println("new string print");
 
-        Class.forName("org.sqlite.JDBC");
         Connection connection;
-        String url = "jdbc:sqlite:/db/database.db";
+        String driver = "jdbc:sqlite:";
+        String dbPath = "Z:\\Общая преподавателей\\Милых Светозар\\rss_reader\\src\\db\\database.db";
+        String url = driver + dbPath;
         connection = DriverManager.getConnection(url);
 
         Statement statement = connection.createStatement();
-        boolean result = statement.execute("CREATE TABLE news (x INT, y INT);");
+        boolean result = statement.execute("CREATE TABLE news (x int, y int);");
         System.out.println("result = " + result);
+
+        ResultSet resultSet = statement.executeQuery("select * from news");
+        resultSet.first();
+
+//        while(resultSet.next()) {
+//            System.out.println(resultSet.);
+//        }
     }
 }
