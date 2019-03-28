071735addd580bdb653401d885eb9d3f458098f8
svetomsk <svt2404@gmail.com>
2019-03-28 19:34:03 +0300

    network helper implemented

--- src/network/NetworkHelper.java
+++ src/network/NetworkHelper.java
@@ -1,7 +1,21 @@
 package network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class NetworkHelper {
-    public static String fetchHTML(String url) {
-        return "";
+    public static String fetchHTML(String url) throws IOException {
+        URL remoteURL = new URL(url);
+        InputStream input = remoteURL.openStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+        String s;
+        StringBuilder builder = new StringBuilder();
+        while ((s = br.readLine()) != null) {
+            builder.append(s);
+        }
+        return builder.toString();
     }
 }
